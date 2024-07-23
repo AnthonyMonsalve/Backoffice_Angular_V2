6aefd5753c6092eb4b173b96dc0e75d92efc9b88
Anthony Monsalve <22anthony.monsalve@gmail.com>
2024-07-23 16:53:14 -0400

    NgbModule en shared

--- src/app/_commerce/commerce.module.ts
+++ src/app/_commerce/commerce.module.ts
@@ -13,15 +13,16 @@ import { CommerceRoutingModule } from './commerce-routing.module';
 import { CardCountComponent } from './presentations/components/card-count/card-count.component';
 import { TerminalsStatusOverviewComponent } from './presentations/components/terminals-status-overview/terminals-status-overview.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AffiliateListComponent } from './presentations/components/affiliate-list/affiliate-list.component';
 import { ClosuresDataChartComponent } from './presentations/components/closures-data-chart/closures-data-chart.component';
+import { ClosuresListComponent } from './presentations/components/closures-list/closures-list.component';
 import { AffiliateDetailComponent } from './presentations/pages/affiliate-detail/affiliate-detail.component';
 import { AffiliateMasterDetailComponent } from './presentations/pages/commerce-detail/commerce-detail.component';
 import { CommerceListComponent } from './presentations/pages/commerce-list/commerce-list.component';
 import { PageAffiliateListComponent } from './presentations/pages/page-affiliate-list/page-affiliate-list.component';
 import { SummaryCommerceComponent } from './presentations/pages/summary/summary.component';
 import { TerminalDetailComponent } from './presentations/pages/terminal-detail/terminal-detail.component';
-import { ClosuresListComponent } from './presentations/components/closures-list/closures-list.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { ClosuresListComponent } from './presentations/components/closures-list/
     NgbPaginationModule,
     NgbTypeaheadModule,
     FormsModule,
+    NgbModule,
   ],
   providers: [DecimalPipe],
 })
